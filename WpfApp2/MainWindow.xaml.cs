cc7b977108d35fb7c832752ef7a50f7d19161d93
Elvin <camalzade_elvin@mail.ru>
2021-08-06 13:12:44 +0400

    Add project files.

---
+++ WpfApp2/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+        }
+        public int Count { get; set; } = 0;
+        private void btn_Click(object sender, RoutedEventArgs e)
+        {
+            ++Count;
+            //btn.Content = Count.ToString();
+        }
+
+        private void repeat_btn_Click(object sender, RoutedEventArgs e)
+        {
+            ++Count;
+            //repeat_btn.Content = Count.ToString();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            //myframe.Source = new Uri($"https://www.google.com/search?q={txtbox1.Text}");
+        }
+    }
+}
